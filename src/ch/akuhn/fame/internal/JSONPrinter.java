6dd26fbbfdea13f6f34a4460516c403a646cea0d
Benoit Verhaeghe <benoit.verhaeghe@berger-levrault.com>
2021-07-27 16:52:20 +0200

    Begin printer thanks to Sabri from berger-levrault

---
+++ src/ch/akuhn/fame/internal/JSONPrinter.java
@@ -0,0 +1,124 @@
+package ch.akuhn.fame.internal;
+
+
+/**
+ * This class a helps printing model in JSON
+ *
+ * @author Sabri.BENBRAHIM, Benoit "badetitou" VERHAEGHE
+ */
+public class JSONPrinter extends AbstractPrintClient {
+
+    /**
+     * Constructor
+     *
+     * @param stream in which we have to write
+     */
+    public JSONPrinter(Appendable stream) {
+        super(stream);
+    }
+
+    @Override
+    public void beginAttribute(String name) {
+        lntabs();
+        append("\"");
+        append(name);
+        append("\"");
+        append(":");
+
+    }
+
+    @Override
+    public void beginDocument() {
+        append("[");
+    }
+
+    @Override
+    public void beginElement(String name) {
+        this.indentation++;
+        append("{");
+        lntabs();
+        append("\"FM3\":\"");
+        append(name);
+        append("\",");
+        lntabs();
+    }
+
+    public void beginMultivalue(String name) {
+        append("[");
+    }
+
+    @Override
+    public void directive(String name, String... params) {
+    }
+
+    @Override
+    public void endAttribute(String name) {
+    }
+
+    @Override
+    public void endDocument() {
+        lntabs();
+        append("]");
+        close();
+    }
+
+    @Override
+    public void endElement(String name) {
+        lntabs();
+        append("}");
+        this.indentation--;
+    }
+
+    public void endMultivalue(String name) {
+        append("]");
+    }
+
+    @Override
+    public void primitive(Object value) {
+        if (value instanceof String) {
+            append('"');
+            append((String) value);
+            append('"');
+        } else {
+            assert false : "Unknown primitive: " + value + " of type: " + value.getClass().getCanonicalName();
+        }
+    }
+
+    @Override
+    public void reference(int index) {
+        append(" ");
+        append("{");
+        append("\"ref\":");
+        append(" ");
+        append(String.valueOf(index));
+        append("}");
+    }
+
+    @Override
+    public void reference(String name) {
+        append(" ");
+        append("{");
+        append("\"ref\":");
+        append(" ");
+        append("\"");
+        append(name);
+        append("\"");
+        append("}");
+    }
+
+    @Override
+    public void reference(String name, int index) {
+    }
+
+    @Override
+    public void serial(int index) {
+        append("\"id\":");
+        append(String.valueOf(index));
+        append(",");
+    }
+
+    public void printEntitySeparator() {
+        append(",");
+    }
+
+}
